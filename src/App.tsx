d57dd5c87a26b4db0b2e5a7136751163faefdf63
balaprogrammer2203 <balaprogrammer2203@gmail.com>
2025-03-21 11:31:50 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="wrapper">
+      <header className="header">
+       
+      </header>
+    </div>
+  );
+}
+
+export default App;
